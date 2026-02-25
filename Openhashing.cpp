d74d75919247521049c86158928af8b9bf0697f4
Tayyaba463 <tayyabakhan773@gmail.com>
2026-02-25 11:40:04 +0500

    Update Openhashing.cpp

--- Openhashing.cpp
+++ Openhashing.cpp
@@ -1,93 +1,93 @@
-
 #include <iostream>
 using namespace std;
 
-struct Node {
-    int data;
-    Node* next;
+#define size 10
+
+struct Node
+{
+    int data = -1;
+    Node *next = NULL;
 };
 
-int size = 10;
-Node HT[10];
+Node hashtable[size];
 
-void initializing() {
-    for (int i = 0; i < size; i++) {
-        HT[i].data = -1;
-        HT[i].next = NULL;
-    }
+void insertData (int value)
+{
+   int index = value % size;
+
+   if (hashtable[index].data == -1)
+   {
+      hashtable[index].data = value;
+      return; 
+ }
+
+  Node *temp = new Node;
+  temp->data = value;
+  temp->next = NULL;
+  if (hashtable[index].next == NULL)
+  {
+    hashtable[index].next = temp;
+    return;
+  }
+ else {
+  Node *current = hashtable[index].next;
+
+  while (current->next != NULL)
+  {
+    current = current->next;
+  }
+  current->next = temp;
+}
 }
 
-void insertValue(int value) {
-
-    int index = value % size;
-    if (HT[index].data == -1) {
-        HT[index].data = value;
-        return;
-    }
-   
-    Node* temp = (Node*) malloc(sizeof(Node));  
-    temp->data = value;
-    temp->next = NULL;
-    if (HT[index].next == NULL) {
-        HT[index].next = temp;
-        return;
-    }
-    Node* current = HT[index].next;
-
-    while (current->next != NULL) {
-        current = current->next;
-    }
-    current->next = temp;
-}
-
-void search (int value) {
-    
+void searchData (int value)
+{
     int index = value % size;
 
-    if (HT[index].data == -1) {
-        cout << "Value not found." << endl;
+    if (hashtable[index].data == -1)
+    {
+        cout << "Hashtable not having the particular value." << endl;
         return;
     }
 
-    Node *current = HT[index].next;
-
-    while (current != NULL) {
-        if (HT[index].data == value) {
+    Node *current = hashtable[index].next;
+    while (current != NULL)
+    {
+        if (current->data == value)
+        {
             cout << "value founded." << endl;
             return;
         }
-        if (current->data == value) {
-            cout << "Value found.";
-            return;
-        }
-        current->next = current;
+        current = current->next;
     }
+
+    cout << "Value not Found." << endl; 
 }
 
-void displaydata() {
+void display ()
+{
+   for (int i = 0; i < size; i++)
+   {
+    cout << "Index -> " << i << "1: ";
+    cout << hashtable[i].data << " ";
+    Node *current = hashtable[i].next;
 
-    for (int i = 0; i < size; i++) {
-        cout << "Index " << i << "1: ";
-        cout << HT[i].data << " ";
-        Node* current = HT[i].next;
-
-        while (current != NULL) {
-            cout << current->data << " ";
-            current = current->next;
-        }
-        cout << endl;
+    while (current != NULL)
+    {
+      cout << current->data << " ";
+      current = current->next;
     }
+    cout << endl;
+   }
 }
-
 
 int main() {
 
     int num, value;
-     initializing();
 
     while (true) {
 
-        cout << "\n1. Insert value" << endl;
+        cout << "1. Insert value" << endl;
         cout << "2. Display Data" << endl;
         cout << "3. search data." << endl;
         cout << "4. Exit" << endl;
@@ -97,15 +97,15 @@ int main() {
         if (num == 1) {
             cout << "Insert value: ";
             cin >> value;
-            insertValue(value);
+            insertData(value);
         }
         else if (num == 2) {
-            displaydata();  
+            display();  
         }
         else if (num == 3) {
          cout << "ENter value to search." << endl;
          cin >> value;
-         search(value);
+         searchData(value);
         }
         else if (num == 4) {
             cout << "You have successfully exited." << endl;
@@ -116,4 +116,5 @@ int main() {
         }
     }
 }
+
 
